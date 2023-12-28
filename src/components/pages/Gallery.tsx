34b35595b21c6e418978cb607b541d8160d01a5a
aritraChatterjee <aritra.cnw@gmail.com>
2023-12-28 16:21:56 +0100

    added gallery dialog

--- src/components/pages/Gallery.tsx
+++ src/components/pages/Gallery.tsx
@@ -1,161 +1,40 @@
 import ImageList from '@mui/material/ImageList';
-import { PageContainer, theme } from '../PageContainer';
+import { PageContainer } from '../PageContainer';
 import ImageListItem from '@mui/material/ImageListItem';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { Dialog, DialogContent } from '@mui/material';
+import { useState } from 'react';
 
 export const Gallery = () => {
+  const images: string[] = [];
+
+  for (let i = 1; i <= 21; i++) {
+    images.push('gallery_(' + i + ')');
+  }
+
+  const [galleryDialogOpen, setGalleryDialogOpen] = useState(false);
+  const [imageSource, setImageSource] = useState('');
+
+  const openGalleryDialog = (imageSource: string) => {
+    setGalleryDialogOpen(true);
+    setImageSource(imageSource);
+  };
+
+  const handleGalleryDialogClose = () => {
+    setGalleryDialogOpen(false);
+    setImageSource('');
+  };
+
   return (
     <PageContainer title="Gallery">
-      <ImageList variant="masonry" cols={3} gap={8}>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(1).jpg')}
-            loading="lazy"
-            alt={'gallery_1'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(2).jpg')}
-            loading="lazy"
-            alt={'gallery_2'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(3).jpg')}
-            loading="lazy"
-            alt={'gallery_3'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(4).jpg')}
-            loading="lazy"
-            alt={'gallery_4'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(5).jpg')}
-            loading="lazy"
-            alt={'gallery_5'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(6).jpg')}
-            loading="lazy"
-            alt={'gallery_6'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(7).jpg')}
-            loading="lazy"
-            alt={'gallery_7'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(8).jpg')}
-            loading="lazy"
-            alt={'gallery_8'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(9).jpg')}
-            loading="lazy"
-            alt={'gallery_9'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(10).jpg')}
-            loading="lazy"
-            alt={'gallery_10'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(11).jpg')}
-            loading="lazy"
-            alt={'gallery_11'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(12).jpg')}
-            loading="lazy"
-            alt={'gallery_12'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(13).jpg')}
-            loading="lazy"
-            alt={'gallery_13'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(14).jpg')}
-            loading="lazy"
-            alt={'gallery_14'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(15).jpg')}
-            loading="lazy"
-            alt={'gallery_15'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(16).jpg')}
-            loading="lazy"
-            alt={'gallery_16'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(17).jpg')}
-            loading="lazy"
-            alt={'gallery_17'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(18).jpg')}
-            loading="lazy"
-            alt={'gallery_18'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(19).jpg')}
-            loading="lazy"
-            alt={'gallery_19'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(20).jpg')}
-            loading="lazy"
-            alt={'gallery_20'}
-          />
-        </ImageListItem>
-        <ImageListItem sx={{ margin: '10px' }}>
-          <img
-            src={require('../../static/photos/gallery_(21).jpg')}
-            loading="lazy"
-            alt={'gallery_21'}
-          />
-        </ImageListItem>
+      <ImageList variant="masonry" cols={3} gap={2}>
+        {images.map((image) => {
+          return (
+            <GalleryItem image={image} openGalleryDialog={openGalleryDialog} />
+          );
+        })}
       </ImageList>
       <Grid item xs={15}>
         <Typography
@@ -177,6 +56,70 @@ export const Gallery = () => {
           </Link>
         </Typography>
       </Grid>
+      <GalleryItemDialog
+        open={galleryDialogOpen}
+        handleClose={handleGalleryDialogClose}
+        src={imageSource}
+      />
     </PageContainer>
+  );
+};
+
+const GalleryItem = (props: any) => {
+  return (
+    <ImageListItem sx={{ margin: '10px' }}>
+      <img
+        src={require(`../../static/photos/${props.image}.jpg`)}
+        loading="lazy"
+        alt={`${props.image}`}
+        onClick={() => props.openGalleryDialog(props.image)}
+      />
+    </ImageListItem>
+  );
+};
+
+export const GalleryItemDialog = (props: any) => {
+  return (
+    <Dialog
+      fullWidth={true}
+      maxWidth={'sm'}
+      open={props.open}
+      onClose={props.handleClose}
+      aria-labelledby="scroll-dialog-title"
+      aria-describedby="scroll-dialog-description">
+      <DialogContent
+        dividers={true}
+        sx={{
+          overflow: 'auto',
+          padding: '5',
+          scrollbarwidth: 'thick',
+          '&::-webkit-scrollbar': {
+            width: '0.4em'
+          },
+          '&::-webkit-scrollbar-track': {
+            background: '#f1f1f1'
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundcolor: '#888'
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            background: '#555'
+          }
+        }}>
+        {props.src === '' ? (
+          ''
+        ) : (
+          <ImageList variant="masonry" cols={1}>
+            <ImageListItem>
+              <img
+                src={require(`../../static/photos/${props.src}.jpg`)}
+                loading="lazy"
+                alt={`${props.src}`}
+              />
+            </ImageListItem>
+          </ImageList>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 };
